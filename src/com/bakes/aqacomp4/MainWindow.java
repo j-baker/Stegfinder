7bd9a71d591c992fda6699b7fde0cf5779c9f41b
James Baker <bakes@ymail.com>
2012-01-04 23:34:18 +0000

    Added mockup of options window. Currently labelled as options window but actually SPAM specific.

--- src/com/bakes/aqacomp4/MainWindow.java
+++ src/com/bakes/aqacomp4/MainWindow.java
@@ -3,17 +3,50 @@
  */
 package com.bakes.aqacomp4;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+
 /**
  * @author bakes
  *
  */
 public class MainWindow {
 
+	private JFrame frame;
+
 	/**
-	 * 
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
 	 */
 	public MainWindow() {
-		// TODO Auto-generated constructor stub
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[]", "[]"));
 	}
 
 }
